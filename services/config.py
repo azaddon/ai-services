18475b65a445ae741aafb70fce57705f43fe1390
azaddon <azad.khan355@gmail.com>
2026-07-06 23:06:49 +0530

    updated config

--- services/config.py
+++ services/config.py
@@ -7,8 +7,8 @@ load_dotenv()
 
 class Config:
 
+    LLM_PROVIDER = os.getenv("LLM_PROVIDER", "openai")
+
     OPENAI_KEY = os.getenv("OPENAI_API_KEY")
 
-    MODEL = os.getenv("OPENAI_MODEL")
-
-    LLM_PROVIDER = os.getenv("LLM_PROVIDER", "openai")
+    MODEL = os.getenv("OPENAI_MODEL", "gpt-4.1-mini")
